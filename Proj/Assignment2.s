514ad08b49c6868bbfcce2d539ebb8843c4de4b3
trl10254 <hamzajkhan@gmail.com>
2014-09-19 17:01:09 -0700

    project perfection

--- Proj/Assignment2.s
+++ Proj/Assignment2.s
@@ -4,18 +4,34 @@
 
 _start:
 
-	MOV R1,  #111    /* Puts 111 into R1 */
-	MOV R2,  #5	  /* Puts 5 into R2 */
-	CMP R1, R2		 
-	BGE branch_equal /* branch to branch only if Z>=5
+	MOV R1, #111          @ r1 ? 1
+	MOV R2, #5            @ r2 ? 5
+	MOV R3, #0            @ Counter
+	MOV R4, #0            @ Swap flag 
+	CMP R1, R2		      @ update the CPSR
+	BGE _Subtract
+	BLT _Swapcheck
 
-branch_different:
-	MOV R0, #4 	  /* R0 < 5 */
-	SWI 0
+_Subrtact:
+	SUB R1, R1, R2        @r0 -=r1
+	ADD R3, R3, #1        @counter++
+	CMP R1, R2            @check a>b again
+	BGE _Subtract         @Go through this loop again
+	BLT _Swapcheck        @If the check does not pass do this 
 
-branch_equal:
-	MOV R0, #5	  /* R0 >= 5 */
-	MOV PC, R0	  /* Put R0 into R15 */
+_Swapcheck:
+	CMP R4, #1
+	BEQ end
+	BNE _Swap 	          
+
+_Swap:
+	MOV R5, R0            @Swaps the function in r0 with r5
+	MOV R0, R3            @Swaps the funcion in r3 with r0
+	MOV R3, R5            @Swaps the function in r5 with r3
+	b end
+
+end:
+	MOV R7, #1
 	SWI 0 
 
 
