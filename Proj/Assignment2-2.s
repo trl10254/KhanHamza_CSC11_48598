c63a66ccf7d7c1e63c95b4c9c8927bd5e38e7186
trl10254 <hamzajkhan@gmail.com>
2014-09-21 17:04:26 -0700

    project

---
+++ Proj/Assignment2-2.s
@@ -0,0 +1,35 @@
+/* Khan, Hamza - Assignment 2 - 48598 */
+
+	.global _start
+
+_start:
+
+	MOV R1, #111          @ r1 ? 1
+	MOV R2, #5            @ r2 ? 5
+	MOV R3, #0            @ Counter
+	MOV R4, #0            @ Swap flag 
+	CMP R1, R2		      @ update the CPSR
+	BGE _Subtract
+	BLT _Swapcheck
+
+_Subrtact:
+	SUB R1, R1, R2        @r0 -=r1
+	ADD R3, R3, #1        @counter++
+	CMP R1, R2            @check a>b again
+	BGE _Subtract         @Go through this loop again
+	BLT _Swapcheck        @If the check does not pass do this 
+
+_Swapcheck:
+	CMP R4, #1
+	BEQ end
+	BNE _Swap 	          
+
+_Swap:
+	MOV R5, R0            @Swaps the function in r0 with r5
+	MOV R0, R3            @Swaps the funcion in r3 with r0
+	MOV R3, R5            @Swaps the function in r5 with r3
+	b end
+
+end:
+	MOV R7, #1
+	SWI 0 
