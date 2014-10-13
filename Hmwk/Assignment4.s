3dd4b38ea273ee7222954c0e97eddb0a1c7a0ee3
trl10254 <hamzajkhan@gmail.com>
2014-10-13 09:40:19 -0700

    Update Assignment4.s

--- Hmwk/Assignment4.s
+++ Hmwk/Assignment4.s
@@ -108,24 +108,4 @@ address_of_return : .word return
 @External 
 .global printf
 .global scanf
-=======
-@Efficient technique for calculating a/b and a%b
-.data
 
-.text
-
-.global main
-
-main:
-	MOV R2 = 11111		@Put 11111 into R2
-	MOV R3 = 5			@Put 5 into R3
-	MOV R4 = 1			@Put 1 into R4
-	MOV R5 = R3			@Put R3 into R5
-	MOV R0 = 0			@Put 0 into R0
-	MOV R1 = R2			@Put R2 into R1
-	
-	
-	
-	
-	
->>>>>>> FETCH_HEAD
