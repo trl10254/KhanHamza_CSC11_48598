fb55ab4f0d59e0586d32a4935282e7a8b5f141a2
trl10254 <hamzajkhan@gmail.com>
2014-10-27 16:55:34 -0700

    p

--- Midterm/Assemblyfiles/MainMenu.s
+++ Midterm/Assemblyfiles/MainMenu.s
@@ -49,15 +49,15 @@ loop:
 	
 	CMP R0, #1
 	BL Problem1
-	BEQ loop
+	BL loop
 	
 	CMP R0, #2
 	BL Problem2
-	BEQ loop
+	BL loop
 	
 	CMP R0, #3
 	BL Problem3
-	BEQ loop
+	BL loop
 
     ADD sp, sp, #+4
     pop {lr} 
